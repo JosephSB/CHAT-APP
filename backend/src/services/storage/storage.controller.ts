7a9f96cff1fc3128777067dda954b90cce9313c9
josephsb <jguku2002@gmail.com>
2022-11-17 15:26:35 -0500

    add route from get image

--- backend/src/services/storage/storage.controller.ts
+++ backend/src/services/storage/storage.controller.ts
@@ -1,17 +1,24 @@
+import config from "../../config/index";
 import { Request, Response } from "express";
 import fs from "fs"
 
 export const getImage = (req: Request, res: Response) => {
-    //const params = req.params;
+    const params = req.params;
+
     try {
-        res.end()
+        if(!fs.existsSync(config.root+'/uploads/'+params.dir)) throw new Error("no existe ese directorio")
+        const image = params.size + "-"+params.id;
+        const ext = params.id.split(".")[1];
+        if(!fs.existsSync(config.root+'/uploads/'+params.dir+'/'+image)) throw new Error("no existe esa imagen")
+
+        var img = fs.readFileSync(config.root+'/uploads/'+params.dir+'/'+image); 
+        res.writeHead(200, {'Content-Type': 'image/'+ext }); 
+        res.end(img, 'binary');
     } catch (error) {
         console.error(error)
-        let message = 'Upps ocurrio un error'
-        if (error instanceof Error) message = error.message
 
-        var img = fs.readFileSync('./logo.gif'); 
-        res.writeHead(200, {'Content-Type': 'image/gif' }); 
+        var img = fs.readFileSync(config.root+'/public/notfound.png'); 
+        res.writeHead(200, {'Content-Type': 'image/png' }); 
         res.end(img, 'binary');
 
         
--- backend/src/services/storage/storage.routes.ts
+++ backend/src/services/storage/storage.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { getImage } from "./storage.controller";
 const storageRouter = express.Router();
 
 storageRouter.get("/", (req, res) => {
     res.send("storage from service chat app");
 });
 
-storageRouter.get("/:dir/:size/:id");
+storageRouter.get("/:dir/:size/:id",getImage);
 
 export default storageRouter
